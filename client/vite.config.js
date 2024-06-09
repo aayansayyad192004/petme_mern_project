c3693c62405b429fbdc23d0ecf027b778fba7046
Aayan Sayyad <102234607+aayansayyad192004@users.noreply.github.com>
2024-06-09 17:05:16 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -11,6 +10,8 @@ export default defineConfig({
       },
     },
   },
-
+  build: {
+    outDir: 'dist', // Specify the output directory as "dist"
+  },
   plugins: [react()],
 });
